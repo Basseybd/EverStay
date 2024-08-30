709fa7c934286be49a880fce1b701baac387db3a
Bassey Duke <bassey.bd@gmail.com>
2024-08-30 10:33:36 -0400

    added a vercel.json and a updated the header icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,27 @@ import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
 import SearchModal from "./components/modals/SearchModal";
+import Link from "next/link";
+import Image from "next/image";
 
 export const metadata = {
   title: "EverStay",
   description: "Marketplace for short- and long-term homestays",
   icons: {
-    icon: "/images/EverStayLogoWhite.png",
+    icon: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/images/EverStayLogoWhite.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/images/EverStayLogoWhite.png",
+      },
+    ],
   },
 };
 
@@ -28,6 +43,16 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <header>
+          <Link href="/">
+            <Image
+              src="/images/EverStayLogoWhite.png"
+              alt="Logo"
+              width={100}
+              height={50}
+            />
+          </Link>
+        </header>
         <ClientOnly>
           <ToasterProvider />
           <SearchModal />
